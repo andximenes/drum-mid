c7fa9216e076e73dc4a19afc0aae9d0aa636fa19
André Ximenes <andre1ximenes92@gmail.com>
2023-01-12 10:57:11 -0300

    Criação da função para executar o audio. Substituição do loop While pelo loop For. Usando as teclas Space e Enter no Drum Mid

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+ 
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log('Elemento não encontrado'); 
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+for(contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const pegaOSomDaTecla = tecla.classList[1];
+    const idAudio = `#som_${pegaOSomDaTecla}`; //template string
+
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+            
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
